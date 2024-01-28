a84698b976800c298732f06445ac781b04a6c607
gahee99 <u1879@sookmyung.ac.kr>
2024-01-29 07:47:02 +0900

    feat: init project

---
+++ src/main/java/com/example/chap01/ExampleApplication.java
@@ -0,0 +1,15 @@
+package com.example.chap01;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class ExampleApplication{
+
+    public static void main(String[] args) {
+
+    }
+}
