40521ad84de79b823771497b0b3697682a6f258b
gahee99 <u1879@sookmyung.ac.kr>
2024-01-31 21:12:40 +0900

    feat: tdd 첫걸음 -  Calculator

---
+++ src/test/java/chap01/CalculatorTest.java
@@ -0,0 +1,15 @@
+package chap01;
+
+import com.example.chap01.Calculator;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class CalculatorTest {
+
+    @Test
+    void plus() {
+        int result = Calculator.plus(1,2);
+        assertEquals(result,3);
+        assertEquals(Calculator.plus(3,5), 8);
+    }
+}
