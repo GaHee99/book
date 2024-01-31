563dbd3dac2b88a327f66faf169b294f237864fb
gahee99 <u1879@sookmyung.ac.kr>
2024-01-31 21:31:03 +0900

    feat: 3-클래스 코드 작성

--- src/main/java/com/example/chap01/PasswordStrengthMeter.java
+++ src/main/java/com/example/chap01/PasswordStrengthMeter.java
@@ -6,6 +6,14 @@ public class PasswordStrengthMeter {
         if(password.length()<8) {
             return PasswordStrength.NORMAL;
         }
+        boolean containsNum = false;
+        for(char c : password.toCharArray()) {
+            if (c>='0' && c<'9') {
+                containsNum = true;
+                break;
+            }
+        }
+        if(!containsNum) return PasswordStrength.NORMAL;
         return PasswordStrength.STRONG;
     }
 }
